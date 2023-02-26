8bdb1ced2c6466e97c365c2d02849ffd140c73d3
Justin Akridge <akridjus@gmail.com>
2023-02-25 20:41:36 -0500

    changes

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,53 +1,58 @@
 import { useState } from "react"
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+//   return (
+//     <div>
+//       Button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
 
-const Button = ({ handleClick, text}) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-}
+// const Button = ({ handleClick, text}) => {
+//   return (
+//     <button onClick={handleClick}>{text}</button>
+//   )
+// }
 
 const App = () => {
-  const [ left, setLeft ] = useState(0);
-  const [ right, setRight ] = useState(0);
-  const [ allClicks, setAll ] = useState([])
+  // const [ left, setLeft ] = useState(0);
+  // const [ right, setRight ] = useState(0);
+  // const [ allClicks, setAll ] = useState([])
+  const [ value, setValue ] = useState(0);
   
+  // const leftClicks = () => {
+  //   setAll(allClicks.concat('L'))
+  //   const updatedLeft = left + 1
+  //   setLeft(updatedLeft)
+  // }
 
-  const leftClicks = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
-  }
-
-  const rightClicks = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
+  // const rightClicks = () => {
+  //   setAll(allClicks.concat('R'))
+  //   const updatedRight = right + 1
+  //   setRight(updatedRight)
+  // }
+  const setToValue = (value) => {
+    console.log(value)
+    setValue(value)
   }
 
   return (
     <div>
-      {left}
+      {/* {left}
       <Button handleClick={leftClicks} text="left" />
       <Button handleClick={rightClicks} text="right" />
       {right}
-      <History allClicks={allClicks} />
+      <History allClicks={allClicks} /> */}
+      <button onClick={() => setToValue(value + 1000)}>increase</button>
+      <button onClick={() => setToValue(0)}>zero</button>
+      <button onClick={() => setToValue(value - 1000)}>decrease</button>
     </div>
   )
 }
