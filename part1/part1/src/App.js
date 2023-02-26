90b5c1191590b3dc25eb0ac91919897afd2aa1f9
Justin Akridge <akridjus@gmail.com>
2023-02-26 16:27:46 -0500

    State change added

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,62 +1,42 @@
-import { useState } from "react"
-
-// const History = (props) => {
-//   if (props.allClicks.length === 0) {
-//     return (
-//       <div>
-//         the app is used by pressing the buttons
-//       </div>
-//     )
-//   }
-//   return (
-//     <div>
-//       Button press history: {props.allClicks.join(' ')}
-//     </div>
-//   )
-// }
-
-// const Button = ({ handleClick, text}) => {
-//   return (
-//     <button onClick={handleClick}>{text}</button>
-//   )
-// }
+import { useState } from 'react'
 
 const App = () => {
-  // const [ left, setLeft ] = useState(0);
-  // const [ right, setRight ] = useState(0);
-  // const [ allClicks, setAll ] = useState([])
-  const [ value, setValue ] = useState(0);
-  
-  // const leftClicks = () => {
-  //   setAll(allClicks.concat('L'))
-  //   const updatedLeft = left + 1
-  //   setLeft(updatedLeft)
-  // }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [ selected, setSelected ] = useState(0)
+  const [ votes, setVotes ] = useState(new Uint8Array(10))
 
-  // const rightClicks = () => {
-  //   setAll(allClicks.concat('R'))
-  //   const updatedRight = right + 1
-  //   setRight(updatedRight)
-  // }
-  const setToValue = (value) => {
-    console.log(value)
-    setValue(value)
+  const setValue = () => {
+    setSelected(Math.floor(Math.random() * 8))
   }
 
+  const getVotes = () => {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+  }
+  
   return (
     <div>
-      {/* {left}
-      <Button handleClick={leftClicks} text="left" />
-      <Button handleClick={rightClicks} text="right" />
-      {right}
-      <History allClicks={allClicks} /> */}
-      <button onClick={() => setToValue(value + 1000)}>increase</button>
-      <button onClick={() => setToValue(0)}>zero</button>
-      <button onClick={() => setToValue(value - 1000)}>decrease</button>
+      <p>{anecdotes[selected]} has {votes[selected]} votes</p>
+      <Button handleClick={() => getVotes()} text="vote" />
+      <Button handleClick={() => setValue()} text="press me" />
     </div>
   )
 }
 
-
-
-export default App;
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+export default App
